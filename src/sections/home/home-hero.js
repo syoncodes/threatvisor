e5ca2061de9c40f63681636dcfeca97e564a7bcf
syoncodes <34947109+syoncodes@users.noreply.github.com>
2023-12-27 11:25:56 -0800

    Update home-hero.js

--- src/sections/home/home-hero.js
+++ src/sections/home/home-hero.js
@@ -237,7 +237,7 @@ export default function HomeHero() {
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}>
   <strong style={{ fontSize: '34px', display: 'inline-block', marginBottom: '10px', whiteSpace: 'nowrap' }}>Hackers are smart, but AI is smarter.</strong>
   <div style={{ fontSize: '24px' }}>
-    Invest in your cybersecurity with Treatvisor, a cybersecurity platform that leverages AI to equip your team with the expertise, tools, and skills to deter cyberattacks.
+    Invest in your cybersecurity with Threatvisor, a cybersecurity platform that leverages AI to equip your team with the expertise, tools, and skills to deter cyberattacks.
   </div>
 </div>
 
