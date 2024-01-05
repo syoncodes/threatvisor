64b5f5d3d2c6f05f11e21322dc871990569d16fc
syoncodes <34947109+syoncodes@users.noreply.github.com>
2024-01-04 20:01:23 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-// sections
 import { HomeView } from 'src/sections/home/view';
-
-// ----------------------------------------------------------------------
 
 export const metadata = {
   title: 'Threatvisor: The AI powered Cybersecurity Platform',
 };
 
 export default function HomePage() {
-  return <HomeView />;
+  return (
+    <div style={{ transform: 'scale(0.8)' }}>
+      <HomeView />
+    </div>
+  );
 }
