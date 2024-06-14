ae3420dc4696917ec04d5f7835e3b8b656f8ba5f
syoncodes <34947109+syoncodes@users.noreply.github.com>
2024-06-14 12:12:03 -0500

    Update post-item-horizontal.js

--- src/sections/blog/post-item-horizontal.js
+++ src/sections/blog/post-item-horizontal.js
@@ -4,44 +4,31 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 // routes
-import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
-import { RouterLink } from 'src/routes/components';
 // hooks
 import { useResponsive } from 'src/hooks/use-responsive';
 // utils
 import { fDate } from 'src/utils/format-time';
-import { fShortenNumber } from 'src/utils/format-number';
 // components
-import { Grid, Typography} from '@mui/material';
-
-import Label from 'src/components/label';
+import { Grid, Typography } from '@mui/material';
 import Image from 'src/components/image';
 import Iconify from 'src/components/iconify';
 import TextMaxLine from 'src/components/text-max-line';
-import CustomPopover, { usePopover } from 'src/components/custom-popover';
 
 // ----------------------------------------------------------------------
-const transformDriveLink = (url) => {
-  const fileId = url.split('/d/')[1].split('/view')[0];
-  return `https://drive.google.com/uc?export=view&id=${fileId}`;
-};
+
 export default function PostItemHorizontal({ post, onSelectPost }) {
-  const popover = usePopover();
   const router = useRouter();
   const mdUp = useResponsive('up', 'md');
-  
+
   const {
     title,
     description,
     date,
     coverUrl,
   } = post;
-  const transformedCoverUrl = transformDriveLink(post.coverUrl);
 
   return (
     <Card sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, marginBottom: 3 }}>
@@ -76,7 +63,7 @@ export default function PostItemHorizontal({ post, onSelectPost }) {
             p: 1,
           }}
         >
-          <Image alt={title} src={transformedCoverUrl} sx={{ height: 1, borderRadius: 1.5 }} />
+          <Image alt={title} src={`data:image/jpeg;base64,${coverUrl}`} sx={{ height: 1, borderRadius: 1.5 }} />
         </Box>
       )}
     </Card>
@@ -89,6 +76,6 @@ PostItemHorizontal.propTypes = {
     date: PropTypes.instanceOf(Date),
     description: PropTypes.string,
     title: PropTypes.string,
-    onSelectPost: PropTypes.func.isRequired,
   }),
+  onSelectPost: PropTypes.func.isRequired,
 };
